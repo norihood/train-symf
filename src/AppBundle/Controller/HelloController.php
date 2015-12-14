dfd23f04c5d449c976f85baee1995e642af8c5b8
norihood <norihood@gmail.com>
2015-12-14 13:26:51 +0700

    upload

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+// src/AppBundle/Controller/HelloController.php
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HelloController
+{
+    /**
+     * @Route("/hello/{name}", name="hello")
+     */
+    public function indexAction($name)
+    {
+        return new Response('<html><body>Hello '.$name.'!</body></html>');
+    }
+}
